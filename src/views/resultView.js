35b5a54a4900ccb31261103b4f7e12d3b4e99c43
mo92othman <mo92othman@gmail.com>
2023-10-21 12:23:53 +0200

    build search function

--- src/views/resultView.js
+++ src/views/resultView.js
@@ -2,44 +2,36 @@ export const createResultElement = (data) => {
   const searchResult = document.getElementById('result');
   searchResult.innerHTML = '';
 
-  const error = 'No data available';
+  // const error = 'No data available';
 
   searchResult.innerHTML = `
-    <div class='info'>
-      <h2>${data.Title ? data.Title : error}</h2>
-      <div class='info-1'>
-        <div>
-          <div class='rating'>
-            <img src="./images/star.png">
-            <h4>${data.imdbRating ? data.imdbRating : 'Unknown'}</h4>
-          </div>
-          <p><strong>Year: </strong>${data.Year ? data.Year : 'Unknown'}</p>
-          <p><strong>Runtime: </strong>${
-            data.Runtime ? data.Runtime : 'Unknown'
-          }</p>
-          <p><strong>Language: </strong>${
-            data.Language ? data.Language : 'Unknown'
-          }</p>
-          <p><strong>Awards: </strong>${
-            data.Awards ? data.Awards : 'Unknown'
-          }</p>
-          <p><strong>Released: </strong>${
-            data.Released ? data.Released : 'Unknown'
-          }</p>
-          <p><strong>Cast:</strong>${data.Actors ? data.Actors : 'Unknown'}</p>
-          <div class='genre'><strong>${
-            data.Genre ? data.Genre : 'Unknown'
-          } </strong></div>
-        </div>
-
-        <img src=${
-          data.Poster ? data.Poster : 'No image available'
-        } class='poster'>
-      </div>
-
-      <p><strong>Plot:</strong>${
-        data.Plot ? data.Plot : 'No plot available'
-      }</p>
+  <div class='info'>
+  <h1 style="text-transform: uppercase;">${data.Title ? data.Title : error}</h1>
+  <div class='info-1'>        
+    <div class='rating'>
+        <img src="../assets/icons/star.png">
+        <h4>${data.imdbRating ? data.imdbRating : 'Unknown'}</h4>
     </div>
+    <p><strong>Year: </strong>${data.Year ? data.Year : 'Unknown'}</p>
+    <p><strong>Runtime: </strong>${data.Runtime ? data.Runtime : 'Unknown'}</p>
+    <p><strong>Language: </strong>${
+      data.Language ? data.Language : 'Unknown'
+    }</p>
+    <p><strong>Awards: </strong>${data.Awards ? data.Awards : 'Unknown'}</p>
+    <p><strong>Released: </strong>${
+      data.Released ? data.Released : 'Unknown'
+    }</p>
+    <p><strong>Cast:</strong>${data.Actors ? data.Actors : 'Unknown'}</p>
+    <div class='genre'><strong>${
+      data.Genre ? data.Genre : 'Unknown'
+    } </strong></div>        
+  </div>
+  <div class='plot'>
+    <p><strong>Plot:</strong>${data.Plot ? data.Plot : 'No plot available'}</p>
+  </div>
+</div>
+<div class='poster'>
+  <img src=${data.Poster ? data.Poster : 'No image available'} class='poster'>
+</div>
   `;
 };
